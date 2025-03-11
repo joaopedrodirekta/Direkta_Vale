85a606296bdc008737252a4b8f1dd26873a9f7e9
Joao.Pedro <joao.pedro@direkta.com.br>
2025-03-11 15:23:35 -0300

    Criando rota temporária para superusuário

--- setup/urls.py
+++ setup/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+
+def create_admin(request):
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser("admin", "admin@email.com", "senha123")
+        return HttpResponse("Superusuário criado com sucesso!")
+    return HttpResponse("Superusuário já existe!")
 
 urlpatterns = [
+    path("create-admin/", create_admin),
     path('admin/', admin.site.urls),
 ]
