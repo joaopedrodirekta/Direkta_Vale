8244e61dbd8b5e39ae1399a3b7a273d47f1bc63c
Joao.Pedro <joao.pedro@direkta.com.br>
2025-03-11 15:29:42 -0300

    Corrigindo rota para criar superusuário

--- setup/urls.py
+++ setup/urls.py
@@ -1,21 +1,5 @@
-"""
-URL configuration for setup project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 
@@ -26,6 +10,6 @@ def create_admin(request):
     return HttpResponse("Superusuário já existe!")
 
 urlpatterns = [
-    path("create-admin/", create_admin),
-    path('admin/', admin.site.urls),
+    path("create-admin/", create_admin),  # Criando superusuário
+    path("admin/", admin.site.urls),  # Ativando Django Admin
 ]
