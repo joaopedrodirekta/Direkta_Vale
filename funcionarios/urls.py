8812c9431730a9125b70d10799f6145edcfde0c5
Joao.Pedro <joao.pedro@direkta.com.br>
2025-03-12 16:31:49 -0300

    Ajustes

--- funcionarios/urls.py
+++ funcionarios/urls.py
@@ -5,5 +5,5 @@ from .views import cadastrar_funcionario
 urlpatterns = [
     path("cadastro/", cadastrar_funcionario, name="cadastrar_funcionario"),
     path("listar/", views.listar_funcionarios, name="listar_funcionarios"),
-    path("ficha/<int:id_funcionario>/", views.ficha_funcionario, name="ficha_funcionario"),  # Exibe a ficha de um funcionário
+    path('funcionarios/ficha/<str:id_funcionario>/', views.ficha_funcionario, name='ficha_funcionario'),
 ]
