549f5b58f73b80e6de7ddcc51f22ce3675111208
Joao.Pedro <joao.pedro@direkta.com.br>
2025-03-12 13:41:43 -0300

    Ajustes

--- setup/settings.py
+++ setup/settings.py
@@ -81,6 +81,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")] 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # Adiciona essa linha
 
 # Se quiser que o Django sirva arquivos estáticos no Render
