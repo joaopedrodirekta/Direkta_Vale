a153bca7540e6c7acd09a3fa27a16b3f1bcb8da7
Joao.Pedro <joao.pedro@direkta.com.br>
2025-03-12 15:53:30 -0300

    Ajustes

--- funcionarios/admin.py
+++ funcionarios/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Funcionario
 
-# Register your models here.
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ('id_funcionario', 'nome_completo', 'cpf', 'departamento', 'status')
+    search_fields = ('nome_completo', 'cpf')
+    list_filter = ('status', 'departamento')
