b2d7718c0f76f745f0586c794ad6092b8409f484
Joao.Pedro <joao.pedro@direkta.com.br>
2025-03-17 10:03:25 -0300

    Inserindo dashboards

--- treinamentos/urls.py
+++ treinamentos/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 
-app_name = "treinamentos"  # Define um namespace para evitar conflitos de URL
-
 urlpatterns = [
     path("cadastrar/", views.cadastrar_treinamento, name="cadastrar_treinamento"),
     path("dashboard/", views.dashboard_treinamentos, name="dashboard_treinamentos"),
