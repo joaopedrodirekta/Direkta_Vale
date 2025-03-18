ea3a6f4d6a39bb321dc312c58b24ccf7f9856db8
Joao.Pedro <joao.pedro@direkta.com.br>
2025-03-18 13:11:04 -0300

    Correcao

--- treinamentos/templatetags/custom_filters.py
+++ treinamentos/templatetags/custom_filters.py
@@ -4,5 +4,5 @@ register = template.Library()
 
 @register.filter
 def replace(value, arg):
-    """ Substitui espaços por um caractere definido (ex: '-' para uso em IDs) """
+    """ Substitui espaços por hífens ou qualquer outro caractere """
     return value.replace(" ", arg)
